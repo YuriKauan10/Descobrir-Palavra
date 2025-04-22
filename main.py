eff83e3bf294507fb95d3184baf18b096cbcbd8f
Yuri Kauan <yuri@dnordibe.com.br>
2025-04-22 17:57:42 -0300

    conditions for added hints

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-palavra = 'Yurizinho'
-palavra = palavra.upper()
+palavra = 'yuri'
+palavra = palavra.lower()
 
 contagemErros = 0
+dicaSolicitar = 0
 print("Digite 0 quando quiser parar")
 while True:
     sugestao = input("Digite sua sugestão: ")
-    sugestao = sugestao.upper()
+    sugestao = sugestao.lower()
 
     contagemErros += 1
+
+    if  contagemErros == 3:
+        print(' ')
+        print("Para dica, digite: 3")
+        print(" ")
+
 
     contPalavras = 0;
     for i in palavra:
@@ -15,11 +22,23 @@ while True:
 
     espacosPalavra = "_ " * contPalavras
 
-    if contagemErros > 3:
-        print(f"Dica {espacosPalavra}" )
+    if sugestao == '3':
+        print(" ")
+        print(f"Dica: {espacosPalavra}" )
+
+    contDnv = 0
+    for i in sugestao:
+        for i2 in palavra:
+            if i == i2:
+                contDnv += 1
+
+    if 2 <= contDnv < 4:
+        print(f"{sugestao} está perto")
+    elif contDnv >= 4:
+        print(f"{sugestao} está muito perto")
 
     if sugestao == palavra:
-        print(f"Você acertou! a palavra é {palavra}")
+        print(f"Você acertou! a palavra é --> {palavra} <--")
         break
 
     parada = 1
